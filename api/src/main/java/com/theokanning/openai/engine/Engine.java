66ff3e2156b5bd831e0a803faf3e3c2441697fe8
VolcanoCookies <franek.kg@gmail.com>
2021-07-14 16:12:15 +0200

    More useful to link people to the engines rather than api endpoint

--- api/src/main/java/com/theokanning/openai/engine/Engine.java
+++ api/src/main/java/com/theokanning/openai/engine/Engine.java
@@ -5,27 +5,28 @@ import lombok.Data;
 /**
  * GPT-3 engine details
  *
- * https://beta.openai.com/docs/api-reference/retrieve-engine
+ * @see <a href="https://beta.openai.com/docs/engines">https://beta.openai.com/docs/engines</a>
  */
 @Data
 public class Engine {
-    /**
-     * An identifier for this engine, used to specify an engine for completions or searching.
-     */
-    public String id;
-
-    /**
-     * The type of object returned, should be "engine"
-     */
-    public String object;
-
-    /**
-     * The owner of the GPT-3 engine, typically "openai"
-     */
-    public String owner;
-
-    /**
-     * Whether the engine is ready to process requests or not
-     */
-    public boolean ready;
+	
+	/**
+	 * An identifier for this engine, used to specify an engine for completions or searching.
+	 */
+	public String id;
+	
+	/**
+	 * The type of object returned, should be "engine"
+	 */
+	public String object;
+	
+	/**
+	 * The owner of the GPT-3 engine, typically "openai"
+	 */
+	public String owner;
+	
+	/**
+	 * Whether the engine is ready to process requests or not
+	 */
+	public Boolean ready;
 }
