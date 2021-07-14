c6c99453967a355c337e3dcaf5fdfade4a819d3b
VolcanoCookies <franek.kg@gmail.com>
2021-07-14 16:13:22 +0200

    Formatting and href for link

--- api/src/main/java/com/theokanning/openai/search/SearchResult.java
+++ api/src/main/java/com/theokanning/openai/search/SearchResult.java
@@ -4,24 +4,27 @@ import lombok.Data;
 
 /**
  * A search result for a single document.
+ * <p>
  *
- * https://beta.openai.com/docs/api-reference/search
+ * @see <a href="https://beta.openai.com/docs/api-reference/search">https://beta.openai.com/docs/api-reference/search</a>
  */
 @Data
 public class SearchResult {
-    /**
-     * The position of this document in the request list
-     */
-    Integer document;
-
-    /**
-     * The type of object returned, should be "search_result"
-     */
-    String object;
-
-    /**
-     * A number measuring the document's correlation with the query.
-     * A higher score means a stronger relationship.
-     */
-    Double score;
+	
+	/**
+	 * The position of this document in the request list
+	 */
+	public Integer document;
+	
+	/**
+	 * The type of object returned, should be "search_result"
+	 */
+	public String object;
+	
+	/**
+	 * A number measuring the document's correlation with the query.
+	 * A higher score means a stronger relationship.
+	 */
+	public Double score;
+	
 }
